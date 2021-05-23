57064c8fc605198f5583c18333717d181962b531
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-05-23 09:31:02 -0300

    implements database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_category (name) VALUES ('COXINHAS MASTERS 100g.');
+INSERT INTO tb_category (name) VALUES ('COXINHAS MINIS - Embalagem Com 10 Unidades De 25g Cada.');
+INSERT INTO tb_category (name) VALUES ('ESPETINHO DA TAW.');
+INSERT INTO tb_category (name) VALUES ('E PRA BEBER?');
+INSERT INTO tb_category (name) VALUES ('ENTRADINHAS.');
