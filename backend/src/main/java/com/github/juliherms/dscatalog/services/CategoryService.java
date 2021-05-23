b91289fb4f6a09935c06f7a67e979436701d585c
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-05-23 09:23:27 -0300

    code review

--- backend/src/main/java/com/github/juliherms/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/github/juliherms/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.github.juliherms.dscatalog.entities.Category;
 import com.github.juliherms.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repo;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return  repo.findAll();
     }
