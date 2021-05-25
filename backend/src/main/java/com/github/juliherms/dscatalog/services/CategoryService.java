df309a2f5f46822192bcda930e62e24605bd1e57
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-05-24 22:27:42 -0300

    implements exception handler and optionals

--- backend/src/main/java/com/github/juliherms/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/github/juliherms/dscatalog/services/CategoryService.java
@@ -2,12 +2,14 @@ package com.github.juliherms.dscatalog.services;
 
 import com.github.juliherms.dscatalog.dtos.CategoryDTO;
 import com.github.juliherms.dscatalog.entities.Category;
+import com.github.juliherms.dscatalog.exceptions.EntityNotFoundException;
 import com.github.juliherms.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -26,5 +28,17 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll(){
         return repo.findAll().stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    }
+
+    /**
+     * Methdo responsible to get category by id
+     * @param id
+     * @return
+     */
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> optional = repo.findById(id);
+        Category category = optional.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(category);
     }
 }
